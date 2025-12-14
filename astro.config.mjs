0cd000e369bd303f6c646ed7d4e867437fa7a681
Huskydog9988 <39809509+Huskydog9988@users.noreply.github.com>
2025-12-14 14:21:00 -0500

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,17 @@
 import { defineConfig } from "astro/config";
 import pagefind from "astro-pagefind";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://books.civinfo.net/",
   integrations: [
     pagefind({
       indexConfig: {
         keepIndexUrl: true,
       },
     }),
+    sitemap(),
   ],
 });
